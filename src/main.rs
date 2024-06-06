42e3a7fc29b627aa7b5a95f5542fd26023c6ae80
wwwhoami <subman450@gmail.com>
2024-06-06 19:51:24 +0300

    type builtin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process;
+use std::{process, str::SplitWhitespace};
 
 fn main() {
     let stdin = io::stdin();
@@ -14,23 +14,12 @@ fn main() {
         let mut splitted_input = input.trim().split_whitespace();
 
         match splitted_input.next() {
-            Some("exit") => {
-                let code = splitted_input.next();
-
-                match code {
-                    Some(code) => {
-                        let code = code.parse::<i32>().unwrap();
-                        process::exit(code);
-                    }
-                    None => {
-                        process::exit(0);
-                    }
-                }
-            }
+            Some("exit") => exit_handler(splitted_input),
             Some("echo") => {
                 let text = splitted_input.collect::<Vec<&str>>().join(" ");
                 println!("{}", text);
             }
+            Some("type") => type_handler(splitted_input),
             _ => {
                 println!("{}: command not found", input.trim());
             }
@@ -40,5 +29,38 @@ fn main() {
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
+    }
+}
+
+fn exit_handler(mut splitted_input: SplitWhitespace) {
+    let code = splitted_input.next();
+
+    match code {
+        Some(code) => {
+            let code = code.parse::<i32>().unwrap();
+            process::exit(code);
+        }
+        None => {
+            process::exit(0);
+        }
+    }
+}
+
+fn type_handler(mut splitted_input: SplitWhitespace) {
+    let command = splitted_input.next().unwrap();
+
+    match command {
+        "exit" => {
+            println!("exit is a shell builtin");
+        }
+        "echo" => {
+            println!("echo is a shell builtin");
+        }
+        "type" => {
+            println!("type is a shell builtin");
+        }
+        _ => {
+            println!("{} not found", command);
+        }
     }
 }
