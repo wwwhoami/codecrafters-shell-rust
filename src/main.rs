699392e4c839775915c418bb5793eed080730bf0
wwwhoami <subman450@gmail.com>
2024-06-06 19:40:01 +0300

    exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     let stdin = io::stdin();
@@ -10,7 +11,28 @@ fn main() {
     io::stdout().flush().unwrap();
 
     while stdin.read_line(&mut input).is_ok() {
-        println!("{}: command not found", input.trim());
+        let mut splitted_input = input.trim().split_whitespace();
+
+        match splitted_input.next() {
+            Some("exit") => {
+                let code = splitted_input.next();
+
+                match code {
+                    Some(code) => {
+                        let code = code.parse::<i32>().unwrap();
+                        process::exit(code);
+                    }
+                    None => {
+                        process::exit(0);
+                    }
+                }
+            }
+            _ => {
+                println!("{}: command not found", input.trim());
+            }
+        }
+
+        // Clear input buffer and print prompt
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
